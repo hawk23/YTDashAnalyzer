265d30bfd8ed256279b3d5583b7bb7bee2fa2ff3
Martin Fillafer <martin.fillafer@bitmovin.net>
2016-02-18 16:27:06 +0100

    refactor csv generation and generate stats for 500 most viewed videos on youtube

--- youtube-csv.py
+++ youtube-csv.py
@@ -3,48 +3,47 @@
 import youtube_dl
 import csv
 
-ydl = youtube_dl.YoutubeDL({'outtmpl': u'%(id)s%(ext)s'})
+ydl = youtube_dl.YoutubeDL({'outtmpl': u'%(id)s%(ext)s', 'ignoreerrors': True})
 
 with ydl:
     result = ydl.extract_info(
-            # 'https://www.youtube.com/playlist?list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC',
-            'https://www.youtube.com/playlist?list=PLDC963AC0FD2A0FCB',
+            'https://www.youtube.com/playlist?list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC',
+            #'https://www.youtube.com/playlist?list=PLDC963AC0FD2A0FCB',
+            #'https://www.youtube.com/watch?v=7zp1TbLFPp8',
             download=False # We just want to extract the info
     )
 
-csvwriter = csv.writer(open('stats.csv', 'wb'), dialect='excel')
+representationWriter = csv.writer(open('representation_stats.csv', 'wb'), dialect='excel')
+representationWriter.writerow(['Youtube ID', 'Video Codec', 'Audio Codec', 'Width', 'Height', 'FPS', 'Extension', 'Format', 'Resolution', 'Bitrate'])
 
-csvwriter.writerow(['Youtube ID', 'Video Codec', 'Audio Codec', 'Width', 'Height', 'FPS', 'Extension', 'Format'])
+videoWriter = csv.writer(open('video_stats.csv', 'wb'), dialect='excel')
+videoWriter.writerow(['Youtube ID', 'Representation Count'])
 
-stats = {
-    'video_rep': {},
-    'vcodec': {},
-    'acodec': {},
-    'format': {}
-}
-
-def increment(stat, key):
-    if key in stats[stat]:
-        stats[stat][key] += 1
-    else:
-        stats[stat][key] = 1
 
 def analyze_video(video):
+    repCount = 0
 
-    for rep in video['formats']:
-        if rep['vcodec'] != u'none':
-            csvwriter.writerow([
-                video['id'],
-                rep.get('vcodec', '-'),
-                rep.get('acodec', '-'),
-                rep.get('width'),
-                rep.get('height'),
-                rep.get('fps', 0),
-                rep.get('ext', '-'),
-                rep.get('format')
-            ])
+    if video is not None and 'formats' in video:
+        for rep in video['formats']:
+            if rep['vcodec'] != u'none':
+                representationWriter.writerow([
+                    video['id'],
+                    rep.get('vcodec', '-').strip(),
+                    rep.get('acodec', '-').strip(),
+                    rep.get('width', 0),
+                    rep.get('height', 0),
+                    rep.get('fps', 0),
+                    rep.get('ext', '-'),
+                    rep.get('format'),
+                    "{0}x{1}".format(rep.get('width', '?'), rep.get('height', '?')),
+                    rep.get('tbr', rep.get('abr', 'none'))
+                ])
 
-if 'entries' in result:
+                repCount += 1
+
+        videoWriter.writerow([video['id'], repCount])
+
+if result is not None and 'entries' in result:
     # Can be a playlist or a list of videos
     for video in result['entries']:
         analyze_video(video)
